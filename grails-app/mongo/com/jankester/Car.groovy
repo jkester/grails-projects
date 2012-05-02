ab366cd0141b5ea38c17edd8cd6015e032cc0dbe
Jan Kester <jan.c.kester@gmail.com>
2012-05-02 21:04:58 +0200

    Adding mongo class to git.

---
+++ grails-app/mongo/com/jankester/Car.groovy
@@ -0,0 +1,18 @@
+package com.jankester
+
+
+class Car {
+	String name
+	String brand
+
+	int ps // horsepower 
+	Date buildDate
+
+	Date dateCreated // autoset by plugin
+	Date lastUpdated // autoset by plugin
+
+	static constraints = {
+		brand nullable:true
+		ps min: 30, max: 1001
+	}
+}
